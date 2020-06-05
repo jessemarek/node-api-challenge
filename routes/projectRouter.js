eb36c0c95f3cfc606fe7180cffeaec7ee61c64dc
unknown <dethgrenade@gmail.com>
2020-06-05 08:35:08 -0700

    POST new project working

--- routes/projectRouter.js
+++ routes/projectRouter.js
@@ -4,8 +4,14 @@ const router = require('express').Router()
 const Projects = require('../data/helpers/projectModel')
 
 
-router.post('/', (req, res) => {
-
+router.post('/', validateProject, (req, res) => {
+    Projects.insert(req.body)
+        .then(project => {
+            res.status(201).json(project)
+        })
+        .catch(err => {
+            res.status(500).json({ errorMessage: "Server failed to create a new project" })
+        })
 })
 
 router.get('/', (req, res) => {
@@ -17,5 +23,17 @@ router.get('/', (req, res) => {
             res.status(500).json({ errorMessage: "Server failed to retrieve list of projects" })
         })
 })
+
+function validateProject(req, res, next) {
+    if (req.body.name && req.body.description) {
+        next()
+    } else if (!req.body.name) {
+        res.status(400).json({ errorMessage: "PLease provide a project name" })
+    } else if (!req.body.description) {
+        res.status(400).json({ errorMessage: "PLease provide a project description" })
+    } else {
+        res.status(400).json({ errorMessage: "PLease provide a project name and description" })
+    }
+}
 
 module.exports = router
